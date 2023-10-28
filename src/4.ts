b3e2650998445e9aa59490907cb12e961f9e92f6
Alexey Hvozduhin <124602564+OleksiiHvozdukhin@users.noreply.github.com>
2023-10-28 13:57:25 +0300

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class Hous {
+  protected tenants: Person[];
+  protected door: boolean;
+  constructor(protected key: Key) {
+    this.door = false;
+  }
+  comeIn(person: Person): void {
+    if ((this.door === true)) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends Hous {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
